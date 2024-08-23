077606b964c806452e6bf2a40967a455fa4ec3ff
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-23 19:18:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// terdffdgn
+const express = require('express');
+const dbcontent = require("./config/db");
+const userRouter = require("./routers/user.rouer")
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.send({ msg: "start" })
+})
+app.use("/user", userRouter)
+app.listen(8520, () => {
+    console.log("listening  on port 8090");
+    dbcontent()
+})
