f36a534d54d4a7d31e4dfe5a58d5e3a1a24b54d4
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-23 19:14:06 +0530

    user pages

---
+++ view/js/singup.js
@@ -0,0 +1,28 @@
+const postData = async (user) => {
+    let req = await fetch("http://localhost:8520/user/signup", {
+        method: 'POST',
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(user)
+    })
+    let res = await req.json()
+    
+    document.cookie=`id = ${res.User._id}`
+    window.location.href = "./index.html"
+}
+
+
+const handleData = (e) => {
+    e.preventDefault();
+    let data = {
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+    }
+
+  postData(data)
+
+
+}
+
+
+document.getElementById("userData").addEventListener("submit", handleData)
